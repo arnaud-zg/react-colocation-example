9ad0848a183949469ddc8b4b873ba8dccae5cc8c
Arnaud Zheng <arnaud-zg@users.noreply.github.com>
2025-11-01 13:21:23 +0100

    fix: use store welcome survey with tanstack store

--- src/ui/components/WelcomeModal/WelcomeModal.survey.ts
+++ src/ui/components/WelcomeModal/WelcomeModal.survey.ts
@@ -3,11 +3,11 @@ import type { WelcomeStorageRepository } from "@/domain/welcomeSurvey/WelcomeSur
 import { useImmutableInstance } from "@/lib/useImmutableInstance";
 import { useSyncExternalStore } from "react";
 // import { LocalStorageWelcomeSurveyRepository } from "./WelcomeStorage/LocalStorageWelcomeSurvey.repository";
-// import { TanStackWelcomeSurveyRepository } from "./WelcomeStorage/TanstackStoreWelcomeSurvey.repository";
-import { ZustandWelcomeSurveyRepository } from "./WelcomeStorage/ZustandWelcomeSurvey.repository";
+import { TanStackWelcomeSurveyRepository } from "./WelcomeStorage/TanstackStoreWelcomeSurvey.repository";
+// import { ZustandWelcomeSurveyRepository } from "./WelcomeStorage/ZustandWelcomeSurvey.repository";
 
 export const welcomeStorage: WelcomeStorageRepository =
-  new ZustandWelcomeSurveyRepository();
+  new TanStackWelcomeSurveyRepository();
 
 export const useWelcomeModalSurvey = () => {
   const welcomeSurvey = useSyncExternalStore(
